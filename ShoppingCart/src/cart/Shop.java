9572a4792f51e7542830fd8b079f25daa536d380
PawarSumukh <109122921+PawarSumukh@users.noreply.github.com>
2023-03-13 16:16:39 +0530

    Add files via upload
    Step 2 completed

--- ShoppingCart/src/cart/Shop.java
+++ ShoppingCart/src/cart/Shop.java
@@ -5,8 +5,27 @@ import java.util.List;
 import java.util.Map;
 
 public class Shop {
-	
+
 	Map<String, Double> itemCostMap = new HashMap<String, Double>();
+	Map<String, Integer> itemCount = new HashMap<String, Integer>();
+	Boolean buyOneGetOneOnApple = false;
+	Boolean buyThreeAtTwoOnOrange = false;
+
+	public Boolean getBuyOneGetOneOnApple() {
+		return buyOneGetOneOnApple;
+	}
+
+	public void setBuyOneGetOneOnApple(Boolean buyOneGetOneOnApple) {
+		this.buyOneGetOneOnApple = buyOneGetOneOnApple;
+	}
+
+	public Boolean getBuyThreeAtTwoOnOrange() {
+		return buyThreeAtTwoOnOrange;
+	}
+
+	public void setBuyThreeAtTwoOnOrange(Boolean buyThreeAtTwoOnOrange) {
+		this.buyThreeAtTwoOnOrange = buyThreeAtTwoOnOrange;
+	}
 
 	public Map<String, Double> getItemCostMap() {
 		return itemCostMap;
@@ -21,11 +40,51 @@ public class Shop {
 		return "Shop [itemCostMap=" + itemCostMap + "]";
 	}
 
-	public Double findTotalCost(List<String> cart) {
-		Double cost=0.0;
-		for(String item:cart) {
-			cost += itemCostMap.get(item);
+	public Double findTotalCost() {
+		Double cost = 0.0;
+		for (String k : itemCount.keySet()) {
+			cost += itemCostMap.get(k) * itemCount.get(k);
 		}
-		return cost;	
-	}	
+		return cost;
+	}
+
+	public Map<String, Integer> updateCart(List<String> cart) {
+		for (String s : cart) {
+			if (!itemCount.containsKey(s)) {
+				itemCount.put(s, 1);
+			} else {
+				itemCount.put(s, itemCount.get(s) + 1);
+			}
+		}
+
+		if (buyOneGetOneOnApple)
+			itemCount.put("apple", updateApple(itemCount.get("apple")));
+		if (buyThreeAtTwoOnOrange)
+			itemCount.put("orange", updateOrange(itemCount.get("orange")));
+
+		return itemCount;
+	}
+
+	public Map<String, Integer> getItemCount() {
+		return itemCount;
+	}
+
+	public void setItemCount(Map<String, Integer> itemCount) {
+		this.itemCount = itemCount;
+	}
+
+	private Integer updateOrange(Integer count) {
+		if (count % 3 == 0)
+			return count / 3 * 2;
+		else
+			return count / 3 * 2 + count % 3;
+	}
+
+	private Integer updateApple(Integer count) {
+		if (count % 2 == 0)
+			return count / 2;
+		else
+			return count / 2 + 1;
+	}
+
 }
--- ShoppingCart/src/cart/ShoppingCart.java
+++ ShoppingCart/src/cart/ShoppingCart.java
@@ -1,5 +1,6 @@
 package cart;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -8,32 +9,43 @@ import java.util.Map;
 public class ShoppingCart {
 
 	public static void main(String[] args) {
-		
-		List<String> cart = Arrays.asList("Apple","Orange","Apple","Apple","xyz");	// add further items in cart for checkout
-		
+
+		List<String> ip = Arrays.asList("Apple", "Orange", "orange", "orangE", "Apple", "Apple", "Apple", "Orange",
+				"orange", "orangE", "Orange"); // add further items in cart for checkout
+
+		Boolean offerOnApple = false;
+		Boolean offerOnOrange = true;
+
 		Map<String, Double> itemCost = new HashMap<String, Double>();
-		itemCost.put("Apple", 0.60);
-		itemCost.put("Orange", 0.25);		
+		itemCost.put("apple", 0.60);
+		itemCost.put("orange", 0.25);
 //		itemCost.put("item3",priceForItem3);	// add further items and prices here
-		
+
 		Double totalCost = 0.0;
 		Shop s = new Shop();
 		s.setItemCostMap(itemCost);
-		
-		
-		
+		s.setBuyOneGetOneOnApple(offerOnApple);
+		s.setBuyThreeAtTwoOnOrange(offerOnOrange);
+
+		List<String> cart = new ArrayList<String>();
+		for (String str : ip) {
+			cart.add(str.toLowerCase());
+		}
+
 		try {
-			for(String item:cart) 
-				if (!itemCost.containsKey(item))
+			for (String item : cart)
+				if (!itemCost.containsKey(item.toLowerCase()))
 					throw new Exception("Invalid Item in cart");
-			totalCost  = s.findTotalCost(cart);
-			System.out.println("items brought: "+cart);
-			System.out.println("Total cost: "+totalCost);
-			
-		}catch(Exception e){
+
+			s.updateCart(cart);
+			totalCost = s.findTotalCost();
+
+			System.out.println("items brought: " + s.getItemCount());
+			System.out.println("Total cost: " + totalCost);
+		} catch (Exception e) {
 			System.out.println("Could not checkout Cart due to the below reason");
 			System.out.println(e.getMessage());
-		}finally {
+		} finally {
 			System.out.println("Thanks for shopping with us... Visit Again!!!");
 		}
 	}
